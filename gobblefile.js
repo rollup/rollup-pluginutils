0bbbf6b73a40050ba677befcc87f5e5991294d9e
Rich-Harris <richard.a.harris@gmail.com>
2015-10-25 19:20:59 -0400

    update deps and build process

--- gobblefile.js
+++ gobblefile.js
@@ -1,21 +1,22 @@
 var gobble = require( 'gobble' );
+var babel = require( 'rollup-plugin-babel' );
 
 module.exports = gobble([
 	// CommonJS build
-	gobble( 'src' ).transform( 'rollup-babel', {
+	gobble( 'src' ).transform( 'rollup', {
 		entry: 'index.js',
 		dest: 'pluginutils.cjs.js',
+		plugins: [ babel() ],
 		format: 'cjs',
-		external: [ 'path', 'minimatch' ],
-		sourceMap: true
+		external: [ 'path', 'minimatch' ]
 	}),
 
 	// ES6 build
-	gobble( 'src' ).transform( 'rollup-babel', {
+	gobble( 'src' ).transform( 'rollup', {
 		entry: 'index.js',
 		dest: 'pluginutils.es6.js',
+		plugins: [ babel() ],
 		format: 'es6',
-		external: [ 'path', 'minimatch' ],
-		sourceMap: true
+		external: [ 'path', 'minimatch' ]
 	})
 ]);
